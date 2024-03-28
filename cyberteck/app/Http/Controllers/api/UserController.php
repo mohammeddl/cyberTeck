db3ce45563a2a2d4eda3e2b1e96281f3719eaac7
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-03-28 15:34:07 +0000

    add faq

---
+++ cyberteck/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(UserRequest $request)
+    {
+        try {
+
+            $validatedData = $request->validated();
+            $validatedData['password'] = Hash::make($validatedData['password']);
+
+            $user = User::create($validatedData);
+            $accessToken = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'access_token' => $accessToken,
+                'user' => $user,
+                'status' => true,
+                'message' => 'user has created Successfully'
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $accessToken,
+            'user' => auth()->user(),
+            'status' => true,
+            'message' => 'valid login'
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out', 'status' => true], 200);
+    }
+}
---
+++ cyberteck/frontend/src/components/faq.jsx
@@ -0,0 +1,58 @@
+export default function Faq(){
+    return(
+        <section className="py-24 bg-gray-50">
+    <div className="max-w-4xl px-8 mx-auto lg:px-16">
+        <h2 className="mb-2 text-xl font-bold text-center md:text-3xl">Frequently Asked Questions</h2>
+
+        <div className="relative mt-2 mt-12 space-y-5">
+
+            <div  className="relative overflow-hidden border-2 border-gray-200 rounded-lg select-none hover:bg-white">
+                <h4  className="flex items-center justify-between text-lg font-medium text-gray-700 cursor-pointer sm:text-xl px-7 py-7 hover:text-gray-800">
+                    <span>Where do I go to upgrade my account?</span>
+                    <svg className="w-6 h-6 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </h4>
+                <p className="pt-0 -mt-2 text-gray-400 sm:text-lg py-7 px-7" >You can upgrade your account by visiting The Pro Upgrade Page. You will also gain access to many other applications and sections of the site.</p>
+            </div>
+
+
+            <div  className="relative overflow-hidden border-2 border-gray-200 rounded-lg select-none hover:bg-white">
+                <h4  className="flex items-center justify-between text-lg font-medium text-gray-700 cursor-pointer sm:text-xl px-7 py-7 hover:text-gray-800">
+                    <span>How do I use Tails in my project?</span>
+                    <svg className="w-6 h-6 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </h4>
+                <p className="pt-0 -mt-2 text-gray-400 sm:text-lg py-7 px-7" >Implementation in your project is very simple. You can use the exported page as a starting point, or you can copy and paste the HTML into your own page.</p>
+            </div>
+
+
+            <div  className="relative overflow-hidden border-2 border-gray-200 rounded-lg select-none hover:bg-white">
+                <h4  className="flex items-center justify-between text-lg font-medium text-gray-700 cursor-pointer sm:text-xl px-7 py-7 hover:text-gray-800">
+                    <span>How long will I have access to Tails?</span>
+                    <svg className="w-6 h-6 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </h4>
+                <p className="pt-0 -mt-2 text-gray-400 sm:text-lg py-7 px-7" >You will have unlimited access to all your pre-built pages; however, if you want to be able to download and export your pages, you'll need a pro account. Paddle for processing payments.</p>
+            </div>
+
+
+             <div  className="relative overflow-hidden border-2 border-gray-200 rounded-lg select-none hover:bg-white">
+                <h4  className="flex items-center justify-between text-lg font-medium text-gray-700 cursor-pointer sm:text-xl px-7 py-7 hover:text-gray-800">
+                    <span>What is the license on the pages?</span>
+                    <svg className="w-6 h-6 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </h4>
+                <p className="pt-0 -mt-2 text-gray-400 sm:text-lg py-7 px-7" >You have unlimited use to the templates used in Tails; however, you cannot re-use the templates to sell for others to use.</p>
+            </div>
+
+
+            <div  className="relative overflow-hidden border-2 border-gray-200 rounded-lg select-none hover:bg-white">
+                <h4  className="flex items-center justify-between text-lg font-medium text-gray-700 cursor-pointer sm:text-xl px-7 py-7 hover:text-gray-800">
+                    <span>What if I need help with my project?</span>
+                    <svg className="w-6 h-6 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </h4>
+                <p className="pt-0 -mt-2 text-gray-400 sm:text-lg py-7 px-7" >If you need assistance implementing the templates into your project you can contact support or you can visit our question section.</p>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+    )
+}
