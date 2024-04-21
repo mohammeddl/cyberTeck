e9336188cfcb82e338f2d9c53dbef65c25f4cbd3
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-04-21 19:00:42 +0100

    add total of product

--- cyberteck/frontend/src/components/ecommerce/ShoppingCarts.jsx
+++ cyberteck/frontend/src/components/ecommerce/ShoppingCarts.jsx
@@ -2,50 +2,22 @@ import { Fragment, useEffect, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { axiosClient } from "../../api/axios";
 
-
-
-const productsold = [
-    {
-        id: 1,
-        name: "Throwback Hip Bag",
-        href: "#",
-        color: "Salmon",
-        price: "$90.00",
-        quantity: 1,
-        imageSrc:
-            "https://wp-pa.phonandroid.com/uploads/2024/02/2-1.jpg",
-        imageAlt:
-            "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-    },
-    {
-        id: 2,
-        name: "Medium Stuff Satchel",
-        href: "#",
-        color: "Blue",
-        price: "$32.00",
-        quantity: 1,
-        imageSrc:
-            "https://assetsio.gnwcdn.com/digitalfoundry-2016-uncharted-4-thiefs-end-tech-analysis-1462720549947.jpg?width=1920&height=1920&fit=bounds&quality=80&format=jpg&auto=webp",
-        imageAlt:
-            "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-    },
-    // More products...
-];
-
 export default function ShoppingCarts() {
     const [open, setOpen] = useState(true);
     const [products, setProducts] = useState([]);
+    const [price, setPrice] = useState(0);
 
     useEffect(() => {
-        // Retrieve cart data from local storage
         const cart = localStorage.getItem("cart")
             ? JSON.parse(localStorage.getItem("cart"))
             : [];
-
-        // Set the products state with the cart data
         setProducts(cart);
+        const total = cart.reduce((acc, item) => {
+            const price = typeof item.price === 'string' ? item.price.replace("$", "") : item.price;
+            return acc + parseFloat(price);
+        }, 0);
+        setPrice(total);
     }, []);
-
 
     return (
         <Transition.Root show={open} as={Fragment}>
@@ -93,7 +65,6 @@ export default function ShoppingCarts() {
                                                         <span className="sr-only">
                                                             Close panel
                                                         </span>
-                                     
                                                     </button>
                                                 </div>
                                             </div>
@@ -115,7 +86,8 @@ export default function ShoppingCarts() {
                                                                     <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                                                         <img
                                                                             src={
-                                                                                product.imageSrc
+                                                                                "http://localhost:8000/images/" +
+                                                                                product.image
                                                                             }
                                                                             alt={
                                                                                 product.imageAlt
@@ -139,9 +111,8 @@ export default function ShoppingCarts() {
                                                                                     </a>
                                                                                 </h3>
                                                                                 <p className="ml-4">
-                                                                                    {
-                                                                                        product.price
-                                                                                    }
+                                                                                    {product.price +
+                                                                                        " USD"}
                                                                                 </p>
                                                                             </div>
                                                                             <p className="mt-1 text-sm text-gray-500">
@@ -160,16 +131,27 @@ export default function ShoppingCarts() {
 
                                                                             <div className="flex">
                                                                                 <button
-                                                                                onClick={() => {
-                                                                                    // Remove product from cart
-                                                                                    const updatedCart = products.filter((item) => item.id !== product.id);
-                                                                                    setProducts(updatedCart);
-                                                                                    
-                                                                                    // Update local storage
-                                                                                    localStorage.setItem("cart", JSON.stringify(updatedCart));
-                                                                                }}
+                                                                                    onClick={() => {
+                                                                                        const updatedCart =
+                                                                                            products.filter(
+                                                                                                (
+                                                                                                    item
+                                                                                                ) =>
+                                                                                                    item.id !==
+                                                                                                    product.id
+                                                                                            );
+                                                                                        setProducts(
+                                                                                            updatedCart
+                                                                                        );
+                                                                                        localStorage.setItem(
+                                                                                            "cart",
+                                                                                            JSON.stringify(
+                                                                                                updatedCart
+                                                                                            )
+                                                                                        );
+                                                                                    }}
                                                                                     type="button"
-                                                                                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                                                                                    className="font-medium text-red-600 hover:text-gray-700"
                                                                                 >
                                                                                     Remove
                                                                                 </button>
@@ -187,7 +169,7 @@ export default function ShoppingCarts() {
                                         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                                             <div className="flex justify-between text-base font-medium text-gray-900">
                                                 <p>Subtotal</p>
-                                                <p>$262.00</p>
+                                                <p>${price.toFixed(2)}</p> 
                                             </div>
                                             <p className="mt-0.5 text-sm text-gray-500">
                                                 Shipping and taxes calculated at
@@ -196,7 +178,7 @@ export default function ShoppingCarts() {
                                             <div className="mt-6">
                                                 <a
                                                     href="#"
-                                                    className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                                                    className="flex items-center justify-center rounded-md border border-transparent bg-gray-900 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-900"
                                                 >
                                                     Checkout
                                                 </a>
