d04c305b7326391d3c34f5ab5bcfe1a9244003fd
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-04-25 20:29:44 +0100

    add pop up form for create blog

---
+++ cyberteck/frontend/src/components/dashboard/blogs/FrormBlog.jsx
@@ -0,0 +1,166 @@
+import { useEffect, useState } from "react";
+
+import { axiosClient } from "../../../api/axios";
+import AddBlogModal from "./AddBlogModal";
+
+export default function FormBlog() {
+    useEffect(() => {}, []);
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        category_id: "",
+        price: "",
+        stock: "",
+        image: null,
+    });
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = new FormData();
+            data.append("name", formData.name);
+            data.append("description", formData.description);
+            data.append("price", formData.price);
+            data.append("stock_quantity", formData.stock);
+            data.append("image", formData.image);
+            data.append("offer", formData.offer);
+            console.log(data);
+
+            const response = await axiosClient.post(
+                "http://localhost:8000/api/products",
+                data,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            if (response.status === 201) {
+                console.log(response.data);
+                console.log("product added successfully");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleI = (e) => {
+        const updatedFormData = { ...formData };
+        updatedFormData[e.target.name] = e.target.value;
+
+        setFormData(updatedFormData);
+    };
+
+    const handelImage = (e) => {
+        const file = e.target.files[0];
+        setFormData((prevData) => ({ ...prevData, image: file }));
+    };
+
+    return (
+        <>
+            <div className="w-full flex justify-center">
+                <button
+                    onClick={() => setIsOpen(true)}
+                    className="mt-6 mx-4 p-2 rounded-lg bg-gray-900 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+                >
+                    Create Blog
+                </button>
+            </div>
+
+            <AddBlogModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <form className="space-y-6 m-12" onSubmit={handleChange}>
+                    <div>
+                        <label
+                            htmlFor="file"
+                            className="block text-sm text-gray-500 dark:text-gray-300"
+                        >
+                            File
+                        </label>
+
+                        <label
+                            htmlFor="dropzone-file"
+                            className="flex flex-col items-center w-full max-w-lg p-5 mx-auto mt-2 text-center bg-white border-2 border-gray-300 border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-8 h-8 text-gray-500 dark:text-gray-400"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                                />
+                            </svg>
+
+                            <h2 className="mt-1 font-medium tracking-wide text-gray-700 dark:text-gray-200">
+                                Payment File
+                            </h2>
+
+                            <p className="mt-2 text-xs tracking-wide text-gray-500 dark:text-gray-400">
+                                Upload or drag & drop your file SVG, PNG, JPG,
+                                or GIF.
+                            </p>
+
+                            <input
+                                id="dropzone-file"
+                                type="file"
+                                className="hidden"
+                                onChange={handelImage}
+                            ></input>
+                        </label>
+                    </div>
+
+                    <div className="col-span-full">
+                        <label
+                            htmlFor="name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={handleI}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-span-full">
+                        <label
+                            htmlFor="description"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Description
+                        </label>
+                        <div className="mt-2">
+                            <textarea
+                                onChange={handleI}
+                                id="description"
+                                name="description"
+                                rows={3}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                defaultValue={""}
+                            />
+                        </div>
+                    </div>
+                    <div className="py-4  ">
+                        <button className="bg-black py-2 px-4 text-white hover:bg-gray-700">
+                            Add Blog
+                        </button>
+                    </div>
+                </form>
+            </AddBlogModal>
+        </>
+    );
+}
--- cyberteck/frontend/src/pages/DashboardBlog.jsx
+++ cyberteck/frontend/src/pages/DashboardBlog.jsx
@@ -1,7 +1,13 @@
+import NavAdmin from "../components/dashboard/NavAdmin";
+import FormBlog from "../components/dashboard/blogs/FrormBlog";
+
 export default function DashboardBlog() {
     return (
-        <div>
-            <h1>Dashboard Blog</h1>
-        </div>
+<div className="relative min-h-[87vh] flex">
+                <NavAdmin />
+                <main className="flex-1 pb-12 space-y-6 overflow-y-auto bg-gray-100  md:space-y-8">
+                <FormBlog/>
+                </main>
+            </div>
     )
 }
